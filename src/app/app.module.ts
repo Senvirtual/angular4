61e4adb2b68724accee2f15117b087138fab1954
Senvirtual <cwbqwert@foxmail.com>
2017-07-11 15:56:37 +0800

    一个网页demo
    Signed-off-by: Senvirtual <cwbqwert@foxmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenueComponent } from './menue/menue.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AnimeComponent } from './content/anime/anime.component';
+import { ManngaComponent } from './content/mannga/mannga.component';
+import { GeemuComponent } from './content/geemu/geemu.component';
+import { OnngakuComponent } from './content/onngaku/onngaku.component';
+import { SliderComponent } from './plugin/slider/slider.component';
+import { RightViewComponent } from './slider/right-view/right-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenueComponent,
+    ContentComponent,
+    FooterComponent,
+    SidebarComponent,
+    AnimeComponent,
+    ManngaComponent,
+    GeemuComponent,
+    OnngakuComponent,
+    SliderComponent,
+    RightViewComponent
   ],
   imports: [
     BrowserModule,
